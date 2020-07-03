260f89762161140ba714c3a27556b4efa7c31723
Cristian Gael Valenzuela Castro <cgvalenzuelac@concredito.com.mx>
2020-07-03 10:28:36 -0600

    .

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,6 +3,8 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 
 export default (req:NextApiRequest, res:NextApiResponse) => {
+  console.log(req);
+  
   res.statusCode = 200
   res.json({ name: 'John Doe' })
 }
